341b39b5a6fa9f5aeaa31a347dc53fb1ac9014d9
Mooffie <mooffie@gmail.com>
2010-03-18 21:36:45 +0200

    [user manual] update
    It isn't yet up-to-date, but it's a start.

--- test/drx_test2.rb
+++ test/drx_test2.rb
@@ -1,19 +1,24 @@
 require 'rubygems'
 require 'dm-core'
 
+#
+# This is part of a blogging website. Users write posts. A post
+# belongs to a user.
+#
+
 class Post
   include DataMapper::Resource
- 
+
   property :post_id,  Serial
   property :title,    String
   property :body,     Text
-  
+
   belongs_to :user
 end
 
 class User
   include DataMapper::Resource
- 
+
   property :user_uid,  Serial
   property :name,      String
   property :mail,      String
@@ -23,4 +28,3 @@ post = Post.new
 
 require 'drx'
 post.see
-

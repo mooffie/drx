7a7cd5e416b05ba426d6247229a62dbad2ebab6f
Mooffie <mooffie@gmail.com>
2010-03-26 14:01:44 +0300

    sync with master.

--- lib/drx/graphviz.rb
+++ lib/drx/graphviz.rb
@@ -4,6 +4,37 @@ module Drx
 
   class ObjInfo
 
+    # Notes:
+    # - Windows' CMD.EXE too supports "2>&1"
+    # - We're generating GIF, not PNG, because that's a format Tk
+    #   supports out of the box.
+    GRAPHVIZ_COMMAND = 'dot "%s" -Tgif -o "%s" -Tcmapx -o "%s" 2>&1'
+
+    @@sizes = {
+      '100%' => "
+        node[fontsize=10]
+      ",
+      '90%' => "
+        node[fontsize=10]
+        ranksep=0.4
+        edge[arrowsize=0.8]
+      ",
+      '80%' => "
+        node[fontsize=10]
+        ranksep=0.3
+        nodesep=0.2
+        node[height=0.4]
+        edge[arrowsize=0.6]
+      ",
+      '60%' => "
+        node[fontsize=8]
+        ranksep=0.18
+        nodesep=0.2
+        node[height=0]
+        edge[arrowsize=0.5]
+      "
+    }
+
     # Create an ID for the DOT node representing this object.
     def dot_id
        ('o' + address.to_s).sub('-', '_')
@@ -21,38 +52,66 @@ module Drx
     end
 
     # Returns the DOT style for the node.
-    def dot_style
+    def dot_style__default
       if singleton?
         # A singleton class
-        "shape=egg,color=lightblue1,style=filled"
+        "shape=oval,color=skyblue1,style=filled"
       elsif t_class?
         # A class
         "shape=oval,color=lightblue1,style=filled"
       elsif t_iclass? or t_module?
         # A module
-        "shape=box,color=aquamarine,style=filled"
+        if repr['#']
+          # Paint anonymous modules only lightly.
+          "shape=box,style=filled,color=\"#D9FFF2\",fontcolor=gray60"
+        else
+          "shape=box,style=filled,color=aquamarine"
+        end
       else
         # Else: a "normal" object, or an immediate.
         "shape=house,color=wheat1,style=filled"
       end
     end
 
-    # Returns the DOT label for the node.
-    def dot_label(max = 20)
-      if class_like?
-        repr
+    # Returns the DOT style for the node.
+    def dot_style__crazy
+      craze = "distortion=#{2*rand-1},skew=#{2*rand-1},orientation=#{360*rand}"
+      crazy_oval = "shape=polygon,sides=25," + craze
+      crazy_rect = "shape=polygon,sides=#{4+rand(3)}," + craze
+      if singleton?
+        # A singleton class
+        "#{crazy_oval},color=palevioletred3,style=filled,fontcolor=white,peripheries=3"
+      elsif t_class?
+        # A class
+        "#{crazy_oval},color=palevioletred1,style=filled"
+      elsif t_iclass? or t_module?
+        # A module
+        "#{crazy_rect},color=peachpuff1,style=filled"
       else
-        # The representation may be quite big, so we trim it.
-        r = repr
-        if r.length > max
-          r[0, max] + ' ...'
-        else
-          r
-        end
+        # Else: a "normal" object, or an immediate.
+        "shape=house,color=pink,style=filled"
       end
     end
 
-    def dot_source(level = 0, &block) # :yield:
+    # Returns the DOT label for the node.
+    #
+    # The representation may be quite big, so we trim it.
+    def dot_label(max = 20)
+      if class_like?
+        # Let's be more lenient when trimming a class/module name.
+        # We want to show The::Last::Component and possibly a singleton's
+        # trailing 'S.
+        max = 60 if max < 60
+      end
+      r = repr
+      if r.length > max
+        r[0, max] + ' ...'
+      else
+        r
+      end
+    end
+
+    def dot_source(level = 0, opts = {}, &block) # :yield:
       out = ''
       # Note: since 'obj' may be a T_ICLASS, it doesn't repond to many methods,
       # including is_a?. So when we're querying things we're using Drx calls
@@ -60,8 +119,7 @@ module Drx
 
       if level.zero?
         out << 'digraph {' "\n"
-        out << '/*size="8,8";*/' "\n"
-        out << 'node [fontsize=10];' "\n"
+        out << @@sizes[opts[:size] || '100%']
         @@seen = {}
       end
 
@@ -69,6 +127,7 @@ module Drx
       @@seen[address] = true
 
       if not seen
+        dot_style = method('dot_style__' + (opts[:style] || 'default')).call
         out << "#{dot_id} [#{dot_style}, label=#{dot_quote dot_label}, URL=#{dot_quote dot_url}];" "\n"
       end
 
@@ -78,7 +137,7 @@ module Drx
 
       if class_like?
         if spr = self.super and display_super?(spr)
-          out << spr.dot_source(level+1, &block)
+          out << spr.dot_source(level+1, opts, &block)
           if [Module, ObjInfo.new(Module).klass.the_object].include? the_object
             # We don't want these relatively insignificant lines to clutter the display,
             # so we paint them lightly and tell DOT they aren't to affect the layout (width=0).
@@ -91,7 +150,7 @@ module Drx
 
       kls = effective_klass
       if display_klass?(kls)
-        out << kls.dot_source(level+1, &block)
+        out << kls.dot_source(level+1, opts, &block)
         out << "#{dot_id} -> #{kls.dot_id} [style=dotted];" "\n"
         out << "{ rank=same; #{dot_id}; #{kls.dot_id}; }" "\n"
       end
@@ -155,14 +214,10 @@ module Drx
     #     system('xview ' + files['gif'])
     #   end
     #
-    def generate_diagram(files, &block)
-      source = self.dot_source(&block)
+    def generate_diagram(files, opts = {}, &block)
+      source = self.dot_source(0, opts, &block)
       File.open(files['dot'], 'w') { |f| f.write(source) }
-
-      # Note: Windows's CMD.EXE too supports "2>&1" (However, this is not
-      # supported on good old Windows 98).
-      command = 'dot "%s" -Tgif -o "%s" -Tcmapx -o "%s" 2>&1' % [files['dot'], files['gif'], files['map']]
-
+      command = GRAPHVIZ_COMMAND % [files['dot'], files['gif'], files['map']]
       message = Kernel.`(command)  # `
       if $? != 0
         error = <<-EOS % [command, message]

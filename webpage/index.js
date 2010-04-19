81b8928db36e78db7b7b72551386de61e2fa2792
Mooffie <mooffie@gmail.com>
2010-04-19 18:11:01 +0300

    [user manual] Added a table of contents.

--- webpage/index.js
+++ webpage/index.js
@@ -12,3 +12,29 @@ $(function() {
     $(elt).html(html)
   });
 })
+
+/**
+ * Generate a Table of Contents.
+ */
+$(function() {
+
+  $('#toc-button').hover(function() { $('#toc').show('fast') })
+  $('#toc').hover(function() {}, function() { $('#toc').hide() })
+
+  var get_indent = function(level) {
+    var s = '';
+    for (var i = 1; i < level; i++) { s += '&nbsp;&nbsp;' }
+    return $('<span>' + s + '</span>');
+  }
+
+  var links = $('<div>')
+  $('h1,h2,h3,h4,h5,h6').each(function(i, elt) {
+    level = elt.tagName.substr(1) * 1
+    get_indent(level).appendTo(links);
+    $('<a>').attr('href', '#' + elt.id).text($(elt).text()).click(function() {
+      $('#toc').hide()
+    }).appendTo(links);
+    $('<br>').appendTo(links);
+  });
+  links.appendTo($('#toc-links'))
+})
